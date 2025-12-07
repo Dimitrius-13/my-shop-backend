bc0fec059e26b472f8b8ffb45774eeecc644a4cb
Dimitrius-13 <dimmmooonnn10i@gmail.com>
2025-12-07 08:45:02 +0200

    updated

--- config/database.ts
+++ config/database.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const parse = require("pg-connection-string").parse;
 
 module.exports = ({ env }) => {
@@ -31,42 +30,6 @@ module.exports = ({ env }) => {
       client: "sqlite",
       connection: {
         filename: ".tmp/data.db",
-=======
-// config/database.js
-
-const parse = require('pg-connection-string').parse;
-
-module.exports = ({ env }) => {
-  // Якщо Strapi знайде змінну DATABASE_URL (це наш Render Postgres), він використає її.
-  if (env('DATABASE_URL')) {
-    const config = parse(env('DATABASE_URL'));
-    return {
-      connection: {
-        client: 'postgres',
-        connection: {
-          host: config.host,
-          port: config.port,
-          database: config.database,
-          user: config.user,
-          password: config.password,
-          // Обов'язково для Render:
-          ssl: {
-            rejectUnauthorized: false
-          },
-        },
-        acquireConnectionTimeout: 60000,
-      },
-    };
-  }
-
-  // Якщо DATABASE_URL не знайдено (наприклад, для локальної розробки),
-  // Strapi повернеться до SQLite, щоб не ламати локальне середовище.
-  return {
-    connection: {
-      client: 'sqlite',
-      connection: {
-        filename: '.tmp/data.db',
->>>>>>> 4e2e856332b0bd98a159ea27fdf38e6b6deb8785
       },
       useNullAsDefault: true,
     },
--- config/plugins.ts
+++ config/plugins.ts
@@ -5,7 +5,6 @@ module.exports = ({ env }) => ({
       // --- БЛОК, ЩО ПРИБИРАЄ ПОПЕРЕДЖЕННЯ ---
       security: {
         // Дозволяємо тільки безпечні типи зображень
-<<<<<<< HEAD
         allowedMimeTypes: [
           "image/jpeg",
           "image/png",
@@ -17,18 +16,10 @@ module.exports = ({ env }) => ({
       // --------------------------------------
 
       provider: "cloudinary",
-=======
-        allowedMimeTypes: ['image/jpeg', 'image/png', 'image/gif', 'image/webp'],
-        maxFileSize: 5 * 1024 * 1024, // Обмеження розміру 5MB
-      },
-      // --------------------------------------
-      
-      provider: 'cloudinary',
->>>>>>> 4e2e856332b0bd98a159ea27fdf38e6b6deb8785
       providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
       },
       actionOptions: {
         upload: {},
