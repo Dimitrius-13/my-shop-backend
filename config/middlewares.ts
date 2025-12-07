b3caae37e8fa598c23726e7b86cfbc4116fbfc8a
Dimitrius-13 <dimmmooonnn10i@gmail.com>
2025-12-07 08:03:32 +0200

    updated

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,66 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::errors",
+  {
+    // Налаштування безпеки
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            "res.cloudinary.com",
+            "megastore-tech.pp.ua",
+            "*.vercel-dns.com",
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            "res.cloudinary.com",
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    // Fix CORS (Дозволяємо фронтенд)
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      headers: "*",
+      origin: [
+        "http://localhost:3000",
+        "https://megastore-tech.pp.ua",
+        "https://www.megastore-tech.pp.ua",
+        "https://megastore-tech-pp-ua.onrender.com", // Замініть на свій реальний домен Render
+      ],
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD"],
+      keepHeaderOnError: true,
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+  {
+    // Fix Payload Limit (Заміна для 'strapi::body' middleware)
+    name: "strapi::body",
+    config: {
+      jsonLimit: "25mb",
+      formLimit: "25mb",
+      textLimit: "25mb",
+      formidable: {
+        maxFileSize: 25 * 1024 * 1024,
+      },
+    },
+  },
 ];
